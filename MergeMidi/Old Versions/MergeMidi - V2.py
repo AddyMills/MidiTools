66a9573745ada4ed917e3e15b5babf668238e83e
AddyMills <74471839+AddyMills@users.noreply.github.com>
2022-02-11 20:22:31 -0500

    New version, can now define which tracks to ignore

--- MergeMidi/Old Versions/MergeMidi - V2.py
+++ MergeMidi/Old Versions/MergeMidi - V2.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 toDelete = ("MARKUP")
-toIgnore = ("VENUE", "BEAT", "EVENTS")
 
 try:
     midRB4 = MidiFile(sys.argv[1], clip = True)
@@ -13,7 +12,7 @@ except IndexError:
 try:
     midRBHP = MidiFile(sys.argv[2], clip = True)
 except:
-    print(f'Upgrade file not found for {os.path.basename(sys.argv[1])}')
+    print(f'RBHP file not found for {os.path.basename(sys.argv[1])}')
     exit()
 
 saveFile = os.path.basename(sys.argv[1])
@@ -29,11 +28,7 @@ for x in midRB4.tracks:
     if x.name in toDelete:
         pass
     elif x.name in upgradeNames:
-        if x.name in toIgnore:
-            tempRB4.add_track()
-            tempRB4.tracks[-1] = x.copy()
-        else:
-            pass
+        pass
     else:
         tempRB4.add_track()
         tempRB4.tracks[-1] = x.copy()
@@ -41,17 +36,9 @@ for x in midRB4.tracks:
 midRB4 = tempRB4
 
 for y, x in enumerate(midRBHP.tracks):
-    if x.name in toIgnore:
-        pass
-    elif x.name in toDelete:
-        pass
-    elif y == 0:
-        pass
-    else:
+    if y != 0:
         midRB4.add_track()
         midRB4.tracks[-1] = x.copy()
-        
-tempRB4 = MidiFile()
 
 try:
     os.mkdir("output")
